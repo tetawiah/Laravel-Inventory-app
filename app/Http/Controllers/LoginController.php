2627e261d7acf9277533e001934748c89a87a957
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-04-16 20:15:40 +0000

    added login functionality

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required',],
+            'password' => ['required','min:8']
+        ]);
+
+      
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+    }
+
+   
+    
+}
