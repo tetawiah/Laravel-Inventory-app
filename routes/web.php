3525785bfad31027b58e392a74267ebcb849d393
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-04-17 11:45:27 +0000

    added middleware group

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\AuthOnly;
+use App\Http\Middleware\GuestOnly;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,24 +20,32 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [InventoryController::class,'index' ]);
 
-Route::post('/create',[InventoryController::class,'store'])->name('products.create'); 
 
-Route::delete('/product/{id}',[InventoryController::class,'delete'])->name('products.delete');
 
-Route::get('/edit/{id}',[InventoryController::class,'updateView'])->name('products.edit.view');
 
-Route::put('/update{id}',[InventoryController::class,'update'])->name('products.update');
+Route::middleware(AuthOnly::class)->group(function () {
+    Route::get('/', [InventoryController::class, 'index'])->name('index');
 
-Route::view('/login','login');
+    Route::post('/create', [InventoryController::class, 'store'])->name('products.create');
 
-Route::post('/login', [LoginController::class,'authenticate'])->name('login');
+    Route::delete('/product/{id}', [InventoryController::class, 'delete'])->name('products.delete');
 
-// Route::middleware(['auth'])->group(function () {
-//     // ...
-// });
+    Route::get('/edit/{id}', [InventoryController::class, 'updateView'])->name('products.edit.view');
 
-// Route::middleware(['guest'])->group(function () {
-//     // ...
-// });
+    Route::put('/update{id}', [InventoryController::class, 'update'])->name('products.update');
+
+    Route::post('/logout', [LogoutController::class, 'logout']);
+});
+
+Route::middleware(GuestOnly::class)->group(function () {
+
+    Route::view('/login', 'login')->name('loginpage');
+
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+
+
+    Route::view('/register', 'register');
+
+    Route::post('/register', [UserController::class, 'register']);
+});
